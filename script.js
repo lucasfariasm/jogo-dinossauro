76c4e0e7feaebf7033c6f37af6d1c7368e61580f
Valdir Moura <lucasfarias.bsb@hotmail.com>
2020-12-04 15:44:27 -0300

    cactus

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32){
-    jump()
+    if(!isJumping){
+      jump()
+    }
   }
 }
 
 function jump() {
   let position = 0;
+
+  isJumping = true;
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -17,6 +23,7 @@ function jump() {
       let downInterval = setInterval(() => {
         if (position<= 0){
           clearInterval(downInterval);
+          isJumping = false;
         }else {
           position -= 20;
           dino.style.bottom = position + 'px';    
@@ -29,4 +36,14 @@ function jump() {
   }, 20);
 }
 
+function createCactus(){
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  
+  cactus.classList.add('cactus');
+  cactus.style.left = 1000 + 'px';
+  background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp)
